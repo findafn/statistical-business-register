c514df480fd05ba3d9fa4ef7b20198f642f0db60
findafn <findafn@gmail.com>
2017-12-28 20:44:31 +0700

    add common buttons

---
+++ src/commons/ButtonSave/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+class ButtonSave extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log('id ',this.props.idSBR);
+  }
+  render() {
+    return (
+        <Button color="info" onClick={this.handleClick} className="detail-btn">Simpan</Button>
+    );   
+  }
+
+}
+
+export default ButtonSave;
