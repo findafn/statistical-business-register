7133fe00787febce15436c84788ca61710d3503a
findafn <findafn@gmail.com>
2017-12-31 01:04:16 +0700

    change button name

--- src/commons/ButtonChange/index.js
+++ src/commons/ButtonChange/index.js
@@ -11,7 +11,7 @@ class ButtonChange extends React.PureComponent {
   }
   render() {
     return (
-        <Button color="info" onClick={this.handleClick} className="detail-btn">Tambah</Button>
+        <Button color="info" onClick={this.handleClick} className="detail-btn">Ubah</Button>
     );   
   }
 
