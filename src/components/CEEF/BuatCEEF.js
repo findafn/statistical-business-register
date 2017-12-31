9d02edf36a88d377e1cbd71c012fc14864da1352
findafn <findafn@gmail.com>
2017-12-31 12:33:21 +0700

    modify checkbox

--- src/components/CEEF/BuatCEEF.js
+++ src/components/CEEF/BuatCEEF.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { Container, Row, Col, NavItem, NavLink, Input, FormGroup, Label } from 'reactstrap';
+import {Checkbox, CheckboxGroup} from 'react-checkbox-group';
 
 class BuatCEEF extends React.Component {
   constructor() {
@@ -10,13 +11,14 @@ class BuatCEEF extends React.Component {
       modal: false,
       namaCEEF: '',
       idCEEF: '0',
-      unitStatistik: '',
-      kodeKBLI: 'A',
+      unitStatistik: [],
+      kodeKBLI: [],
     };
     this.toggleModal = this.toggleModal.bind(this);
     this.onChangeNama = this.onChangeNama.bind(this);
     this.handleOptStatistik = this.handleOptStatistik.bind(this);
     this.handleOptKode = this.handleOptKode.bind(this);
+   
   }
 
   toggleModal() {
@@ -32,18 +34,15 @@ class BuatCEEF extends React.Component {
   }
   handleOptStatistik(e) {
     this.setState(p => ({
-      ...p,
-      unitStatistik: e.target.value,
+      unitStatistik: e,
     }));
     console.log('You have selected:', this.state.unitStatistik);
-    e.persist();
   }
   handleOptKode(e) {
     this.setState({
-      kodeKBLI: e.target.value,
+      kodeKBLI: e,
     });
     console.log('You have selected:', this.state.kodeKBLI);
-    e.persist();
   }
 
   render() {
@@ -83,163 +82,47 @@ class BuatCEEF extends React.Component {
                     </Input>
                   </FormGroup>
                   <p>Pilih unit statistik (bisa >1):</p>
-               
-                    <Col sm={10}>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" value="1" checked={this.state.unitStatistik === '1'} onChange={this.handleOptStatistik} name="checkbox2" />{' '}
-                          Enterprise Group
-                      </Label>
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" value="2" checked={this.state.unitStatistik === '2'} onChange={this.handleOptStatistik} name="checkbox3" />{' '}
-                          Unit Pengunjung
-                      </Label>
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" value="3" checked={this.state.unitStatistik === '3'} onChange={this.handleOptStatistik} name="checkbox4" />{' '}
-                          Enterprise
-                      </Label>
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" value="4" checked={this.state.unitStatistik === '4'} onChange={this.handleOptStatistik} name="checkbox5" />{' '}
-                          Establishment
-                      </Label>
-                      </FormGroup>
-                    </Col>
-               
+                  <CheckboxGroup
+                    name="unitStatistik"
+                    value={this.state.unitStatistik}
+                    onChange={this.handleOptStatistik}>
+
+                    <label><Checkbox value="1" /> Enterprise Group</label>
+                    <label><Checkbox value="2" /> Unit Penunjang</label><br />
+                    <label><Checkbox value="3" /> Enterprise</label><br />
+                    <label><Checkbox value="4" /> Establishment</label>
+                  </CheckboxGroup>
                 </Col>
               </Row>
               <Row>
                 <Col>
                   <p>Pilih kategori KBLI (bisa >1):</p>
-                  <FormGroup tag="fieldset" row>
-                    <Col sm={10}>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="B" checked={this.state.kodeKBLI === 'B'} onChange={this.handleOptKode} name="B" />{' '}
-                          B
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="C" checked={this.state.kodeKBLI === 'C'} onChange={this.handleOptKode} name="C" />{' '}
-                          C
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="D" checked={this.state.kodeKBLI === 'D'} onChange={this.handleOptKode} name="D" />{' '}
-                          D
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="E" checked={this.state.kodeKBLI === 'E'} onChange={this.handleOptKode} name="E" />{' '}
-                          E
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="F" checked={this.state.kodeKBLI === 'F'} onChange={this.handleOptKode} name="F" />{' '}
-                          F
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="G" checked={this.state.kodeKBLI === 'G'} onChange={this.handleOptKode} name="G" />{' '}
-                          G
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="H" checked={this.state.kodeKBLI === 'H'} onChange={this.handleOptKode} name="H" />{' '}
-                          H
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="I" checked={this.state.kodeKBLI === 'I'} onChange={this.handleOptKode} name="I" />{' '}
-                          I
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="J" checked={this.state.kodeKBLI === 'J'} onChange={this.handleOptKode} name="J" />{' '}
-                          J
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="K" checked={this.state.kodeKBLI === 'K'} onChange={this.handleOptKode} name="K" />{' '}
-                          K
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="L" checked={this.state.kodeKBLI === 'L'} onChange={this.handleOptKode} name="L" />{' '}
-                          L
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="M" checked={this.state.kodeKBLI === 'M'} onChange={this.handleOptKode} name="M" />{' '}
-                          M
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="N" checked={this.state.kodeKBLI === 'N'} onChange={this.handleOptKode} name="N" />{' '}
-                          N
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="O" checked={this.state.kodeKBLI === 'O'} onChange={this.handleOptKode} name="O" />{' '}
-                          O
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="P" checked={this.state.kodeKBLI === 'P'} onChange={this.handleOptKode} name="P" />{' '}
-                          P
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="Q" checked={this.state.kodeKBLI === 'Q'} onChange={this.handleOptKode} name="Q" />{' '}
-                          Q
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="R" checked={this.state.kodeKBLI === 'R'} onChange={this.handleOptKode} name="R" />{' '}
-                          R
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="S" checked={this.state.kodeKBLI === 'S'} onChange={this.handleOptKode} name="S" />{' '}
-                          S
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="T" checked={this.state.kodeKBLI === 'T'} onChange={this.handleOptKode} name="T" />{' '}
-                          T
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Label check>
-                          <Input type="checkbox" value="U" checked={this.state.kodeKBLI === 'U'} onChange={this.handleOptKode} name="U" />{' '}
-                          U
-                        </Label>
-                      </FormGroup>
-                    </Col>
-                  </FormGroup>
+                  <CheckboxGroup
+                    name="kodeKBLI"
+                    value={this.state.kodeKBLI}
+                    onChange={this.handleOptKode}>
+
+                    <label><Checkbox value="B" /> B</label>{' '}
+                    <label><Checkbox value="C" /> C</label>{' '}
+                    <label><Checkbox value="D" /> D</label>{' '}
+                    <label><Checkbox value="E" /> E</label>{' '}
+                    <label><Checkbox value="F" /> F</label>{' '}
+                    <label><Checkbox value="G" /> G</label>{' '}
+                    <label><Checkbox value="H" /> H</label>{' '}
+                    <label><Checkbox value="I" /> I</label>{' '}
+                    <label><Checkbox value="J" /> J</label>{' '}
+                    <label><Checkbox value="K" /> K</label>{' '}
+                    <label><Checkbox value="L" /> L</label><br />
+                    <label><Checkbox value="M" /> M</label>{' '}
+                    <label><Checkbox value="N" /> N</label>{' '}
+                    <label><Checkbox value="O" /> O</label>{' '}
+                    <label><Checkbox value="P" /> P</label>{' '}
+                    <label><Checkbox value="Q" /> Q</label>{' '}
+                    <label><Checkbox value="R" /> R</label>{' '}
+                    <label><Checkbox value="S" /> S</label>{' '}
+                    <label><Checkbox value="T" /> T</label>{' '}
+                    <label><Checkbox value="U" /> U</label>{' '}
+                  </CheckboxGroup>
                 </Col>
               </Row>
             </Container>
