13c203b34756deab205d5f32d5e3da85e775a6c7
adyanf <adyanaufalf@gmail.com>
2018-01-04 03:46:58 +0700

    adding logo sbr

--- src/commons/Header/index.js
+++ src/commons/Header/index.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { Row, Col } from 'reactstrap';
 import Container from 'semantic-ui-react/dist/commonjs/elements/Container/Container';
 
+import logo from '../../assets/sbr.png'
+
 class Header extends React.PureComponent {
   render() {
     return (
       <Container>
-          <Row>
+          {/* <Row>
               <Col className="loc-center">Selamat datang Statistical Bussines Register</Col>
-          </Row>
+          </Row> */}
           <Row>
           <Col className="loc-center">
-            <h3>Logo SBR</h3>
+            <img style={{marginTop: "15px"}}src={logo} width="220px"/>
           </Col>
           </Row>
       </Container>
